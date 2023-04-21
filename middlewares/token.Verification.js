779d02103d026a8841b69f9aaccd9bdee5ece68f
[Saurabh Pande] <[sp832154@gmail.com]>
2023-04-21 15:23:58 +0530

    tokenverfication.js file is updated with some more code

--- middlewares/token.Verification.js
+++ middlewares/token.Verification.js
@@ -1,27 +1,31 @@
-const jwt = require('jsonwebtoken');
-const authConfig = require('../configs/auth.config');
 require('dotenv').config(); 
+const jwt = require('jsonwebtoken');
+
+const authConfig = require('../configs/auth.config');
 const constants = require('../utils/constants');
-const fetch = require('../helper/commonfetchingfunction'); 
+const fetch = require('../helper/commonfetchingfunction');
+const time = require('../models/ticket.model');
 
 
-exports.resetPassword = async (req, res) => 
+exports.resetPassword = async (req, res, next) => 
 {
     const token = await req.params.token;
-    /// console.log(token);
+    // console.log(token);
+    
     try
     {
         jwt.verify(token, authConfig.secret, async (err, decoded) =>
         {
             if(err)
             {
+                console.log('Error', err.message)
                 return res.status(401).send
                 ({
                     message : "The link is not valid!. Please check once again."
                 });
             }
-
-            if(decoded.purpose != constants.purpose.Passwordreset)
+            
+            else if(decoded.purpose != constants.purpose.Passwordreset)
             {
                 return res.status(401).send
                 ({
@@ -29,38 +33,62 @@ exports.resetPassword = async (req, res) =>
                 });
             }
 
-            let userDetail = await fetch.getUserDetailsByIdCondition(decoded.id)
+            else if(decoded.resetDone == true)
+            {
+                console.log(`This link is already used for reseting the password. Please make a request again`)
+                return res.status(401).send
+                ({
+                    message : "This link is already used for reseting the password. Please make a request again"
+                });
+            }
+
+            let userDetail = await fetch.getUserDetailsByIdCondition(decoded.id);
             // console.log(userDetail);
 
             if(userDetail)
             {   
-                //if user exists
-                // if(userDetail[0].email_verified == constants.status.notverified)
-                // {
-                //     //if user is already verified
-                //     return res.status(401).send
-                //     ({
-                //         code : 401,
-                //         success : false,
-                //          message : "The user is not verified"
-                //     });
-                // }
-            
-                const dateNow = new Date();
-                console.log(dateNow.getTime());
-
-                const tokenLife = (decoded.iat) * 1000 ;  //time converted to miliseconds
-                console.log(tokenLife);
-
-                if(tokenLife => dateNow.getTime())
-                {   
-                    // checks if token is more then certain time old
-                    // sendEmail.resetPassword(req.user);    // if token is expired, a new token is sent to email-Id
+                // if user exists
+                if(userDetail[0].email_verified == constants.status.verified)
+                {
+                    //if user is already verified
                     return res.status(401).send
                     ({
-                        message : "This link has expired. A new link has been sent to your email address."
+                        code : 401,
+                        success : false,
+                         message : "The user is not verified"
                     });
                 }
+                else
+                {
+                    const tokenCreatedAt = time.convertUnixTimeIntoSimpleFormat(decoded.iat) ;  //time converted to miliseconds
+                    // console.log('This is created time of Token - ', tokenCreatedAt);
+                    
+                    const tokenExpiresAt = time.convertUnixTimeIntoSimpleFormat(decoded.exp);                
+                    // console.log('Token expiration time :- ', tokenExpiresAt);
+
+                    const now = time.nowd();
+                    // console.log(`Current time - `, now)
+                    
+                    if(tokenExpiresAt <= now)
+                    {   
+                        // checks if token is more then certain time old
+                        // sendEmail.resetPassword(req.user); // if token is expired, a new token is sent to email-Id
+                        return res.status(401).send
+                        ({
+                            message : "This link has expired. A new link has been sent to your email address."
+                        }); 
+                    }
+                    else
+                    {
+                        const newTokenDetails = {
+                            tokenCreatedAt : tokenCreatedAt,
+                            id : userDetail[0].id
+                        }
+
+                        next(newTokenDetails);
+
+                    }
+                }     
             }
             else
             {
@@ -74,8 +102,14 @@ exports.resetPassword = async (req, res) =>
     catch(err)
     {
         console.log("#### Error while velidating reset password token ##### ", err.message);
-        res.status(500).send({
+        res.status(500).send
+        ({
             message : "Internal server error while token validation"
         });        
     }
 };
+
+/**
+ * the Unix timestamp used in JSON Web Tokens (JWT) is calculated by counting the number
+ * of seconds that have passed or gone since January 1, 1970, at 00:00:00 UTC. This is also known as the Unix epoch.
+ */

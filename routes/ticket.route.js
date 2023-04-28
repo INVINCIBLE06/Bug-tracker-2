dd249459620b93b66e411c9741d87d8826f811a3
[Saurabh Pande] <[sp832154@gmail.com]>
2023-04-28 12:13:18 +0530

    remove one route which is not for used.It will do the update of ticket after every two but we have to operate manually. if doesnot function by self

--- routes/ticket.route.js
+++ routes/ticket.route.js
@@ -8,8 +8,6 @@ module.exports = function(app)
     app.post('/create/ticket/new', ticketcontroller.AddNewTicket);    
     // through the below ticket we will get all the ticket available in the database
     app.get('/get/all/ticket', ticketcontroller.GetAllTic);
-    // The below route will give all the expired ticket data
-    app.get('/get/all/expired/ticket', ticketcontroller.GetExpiredTicket);
     // the below route will give all the which were asssignn to a particular enginee on the basisi of params
     app.get('/get/all/assigned/ticket/:assignee', verifyparams.CheckAssigneeEmailIsCorrectInParams, ticketcontroller.GetAssignedTicket);
     // the below route will give all the ticket which were created by a particular user on the basis of email id in the params
@@ -22,7 +20,7 @@ module.exports = function(app)
     app.get('/changed/ticket/closed/:id', verifyparams.TicketIdIsCorrectInParams, ticketcontroller.AdminWillChangeTheTicketStatusToClose);
     // The below route will give us back all the tickets where work is not started and there priority is already set to URGENT
     app.get('/notstatrtedworking/priority/urgent/ticket', ticketcontroller.PrioritySetToUrgentbutWorkNotStarted);
-    // The below route will return us back all the urgent priority ticket
+    // The below ro ute will return us back all the urgent priority ticket
     app.get('/urgent/priority/tickets', ticketcontroller.GetAllUrgentPriorityTicket);
     // The below route will give us back the particular ticket report
     app.get('/ticket/report/:id', verifyparams.UserIdIsCorrectInParams, ticketcontroller.ParticularUserTicketActivityReport);
